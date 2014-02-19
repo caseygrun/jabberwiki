9f0c6efee8327efb9cd604310c5f52a0d10c3e3c
Casey Grun <casey.grun@gmail.com>
2014-02-19 00:21:32 -0500

    Implemented real user authentication

--- core/auth.js
+++ core/auth.js
@@ -6,41 +6,40 @@ var util = require('util'),
     passportLocalMongoose = require('passport-local-mongoose');
 
 
-// var AccountSchema = new Schema({
-//     nickname: String,
-//     birthdate: Date
+var AccountSchema = new Schema({
+    name: String,
+});
+AccountSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+Account = mongoose.model('Account',AccountSchema)
+
+passport.use(Account.createStrategy());
+
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// Connect mongoose
+mongoose.connect('mongodb://localhost/jabberwiki');
+
+
+
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     if (username && password) {
+//     	done(null, { email: username })
+//     } else {
+//     	done(null, false, 'Incorrect username or password')
+//     }
+//   }
+// ));
+
+// // authentication
+// passport.serializeUser(function(user, done) {
+//   done(null, user.email);
 // });
-// AccountSchema.plugin(passportLocalMongoose);
-// Account = mongoose.model('Account',AccountSchema)
 
-// passport.use(Account.createStrategy());
-
-// passport.serializeUser(Account.serializeUser());
-// passport.deserializeUser(Account.deserializeUser());
-
-// // Connect mongoose
-// mongoose.connect('mongodb://localhost/jabberwiki');
-
-
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if (username && password) {
-    	done(null, { email: username })
-    } else {
-    	done(null, false, 'Incorrect username or password')
-    }
-  }
-));
-
-// authentication
-passport.serializeUser(function(user, done) {
-  done(null, user.email);
-});
-
-passport.deserializeUser(function(email, done) {
-  done(null, {email: email})
-});
+// passport.deserializeUser(function(email, done) {
+//   done(null, {email: email})
+// });
 
 
 
@@ -50,43 +49,43 @@ module.exports.configure = function(app) {
 	app.use(passport.session());
 
 
-	// app.get('/register', function(req, res) {
- //        res.render('register', { });
- //    });
+	app.get('/register', function(req, res) {
+        res.render('register', { });
+    });
 
- //    app.post('/register', function(req, res) {
- //        Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
- //            if (err) {
- //                return res.render('register', { account : account });
- //            }
+    app.post('/register', function(req, res) {
+        Account.register(new Account({ name : req.body.name, email: req.body.email }), req.body.password, function(err, account) {
+            if (err) {
+                return res.render('register', { account : account });
+            }
 
- //            res.redirect('/');
- //        });
- //    });
+            res.redirect('/');
+        });
+    });
 
- //    app.get('/login', function(req, res) {
- //        res.render('login', { user : req.user });
- //    });
+    app.get('/login', function(req, res) {
+        res.render('login', { user : req.user });
+    });
 
- //    app.post('/login', passport.authenticate('local'), function(req, res) {
- //        res.redirect('/');
- //    });
+    app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: false }), function(req, res) {
+        res.redirect('/');
+    });
 
- //    app.get('/logout', function(req, res) {
- //        req.logout();
- //        res.redirect('/');
- //    });
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
 
     // configure routes
-	app.get('/login',function(req,res,next) {
-		res.render('login.jade')
-	})
-	app.post('/login',
-	  passport.authenticate('local', { successRedirect: '/',
-	                                   failureRedirect: '/login',
-	                                   failureFlash: true })
-	);
+	// app.get('/login',function(req,res,next) {
+	// 	res.render('login.jade')
+	// })
+	// app.post('/login',
+	//   passport.authenticate('local', { successRedirect: '/',
+	//                                    failureRedirect: '/login',
+	//                                    failureFlash: true })
+	// );
 }
 
 
