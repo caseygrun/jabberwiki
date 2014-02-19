5e5906c1e0a4490aff0701ba84af86d80794bb06
Casey Grun <casey.grun@gmail.com>
2014-02-18 22:19:17 -0500

    Added auto-completion of page names

--- public/js/script.js
+++ public/js/script.js
@@ -5,4 +5,36 @@ $(document).ready(function() {
 		selectors: "h3,h4,h5,h6",
 		showAndHide: true,
 	}).data("toc-tocify");
+
+
+	pages = new Bloodhound({
+		datumTokenizer: function(d) { 
+			return [d]; 
+		},
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		limit: 10,
+		prefetch: {
+			url: '/api/all',
+			ttl: 0,
+			filter: function (pages) {
+				return _.map(pages, function (d) {
+					return d.path.substr(1)
+				})
+			}
+		},
+	});
+	 
+	pages.initialize();
+	 
+	// $('#search').typeahead(null, {
+	// 	name: 'pages',
+	// 	displayKey: 'path',
+	// 	source: pages.ttAdapter()
+	// });
+	// 
+	$("#search").typeahead({
+		items: 4,
+		source: pages.ttAdapter()
+	})
+
 });
