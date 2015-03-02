3aa8319447d6448cd452758bb46f92f526a22197
Casey Grun <casey.grun@gmail.com>
2015-03-01 18:09:45 -0600

    Updated app to fix routing

--- app.coffee
+++ app.coffee
@@ -1,169 +1,215 @@
-
+###*
+# Module dependencies.
 ###
-Module dependencies.
-###
 
-# all environments
+http = require('http')
+path = require('path')
 
-# view helpers
+express = require('express')
+_ = require('underscore')
+moment = require('moment')
+passport = require('passport')
+LocalStrategy = require('passport-local').Strategy
+bunyan = require('bunyan')
+expressValidator = require('express-validator')
 
-# authentication
+pages = require('./core/page')
+routes = require('./routes')
+config = require('./config/config')
+revisionary = require('revisionary')
+wiki = require('wiki')
+markup = require('markup')
+authentication = require('./core/auth')
 
-# normal error-handling middleware
-
-# development only
-
-# authentication
-auth = (format) ->
-	format or (format = "html")
-	switch format
-		when "html"
-		else
-			(req, res, next) ->
-				if req.isAuthenticated()
-					res.locals.user = req.user
-					return next()
-				res.redirect "/login"
-
-express = require("express")
-routes = require("./routes")
-config = require("./config/config")
-expressValidator = require("express-validator")
-bunyan = require("bunyan")
-_ = require("underscore")
-passport = require("passport")
-LocalStrategy = require("passport-local").Strategy
-store = require("revisionary")
-wiki = require("wiki")
-markup = require("markup")
-http = require("http")
-path = require("path")
 app = express()
 
-
-app.set "port", process.env.PORT or 3000
-app.set "views", __dirname + "/views"
-app.set "view engine", "jade"
-app.set "log", bunyan.createLogger(
-	name: "wiki"
+# all environments
+app.set 'port', process.env.PORT or 3000
+app.set 'views', __dirname + '/views'
+app.set 'view engine', 'jade'
+app.set 'log', bunyan.createLogger(
+	name: 'wiki'
 	streams: [
-		path: "app.log"
-	,
-		stream: process.stderr
-		level: "info"
-	]
-)
+		{ path: 'app.log' }
+		{
+			stream: process.stderr
+			level: 'info'
+		}
+	])
+
+# view helpers
 app.locals
 	_: _
 	wiki: wiki
 	markup: markup
+	moment: moment
 
-passport.use new LocalStrategy((username, password, done) ->
-	if username and password
-		done null,
-			email: username
-
-	else
-		done null, false, "Incorrect username or password"
-)
+# middleware
 app.use express.favicon()
-app.use express.logger("dev")
-app.use express.static(path.join(__dirname, "public"))
+app.use express.logger('dev')
+app.use express.static(path.join(__dirname, 'public'))
 app.use express.bodyParser()
 app.use expressValidator()
 app.use express.methodOverride()
-app.use express.cookieParser("your secret here")
+app.use express.cookieParser('your secret here')
 app.use express.session()
-app.use passport.initialize()
-app.use passport.session()
+authentication.configure app
 app.use app.router
-app.use require("less-middleware")(src: __dirname + "/public")
+app.use require('less-middleware')(src: __dirname + '/public')
+
+# normal error-handling middleware
 app.use (err, req, res, next) ->
-	app.get("log").error err
-	res.render "error.jade"
+	app.get('log').error err
+	res.render 'error.jade', err
+	return
 
-app.use express.errorHandler()	if "development" is app.get("env")
-passport.serializeUser (user, done) ->
-	done null, user.email
-
-passport.deserializeUser (email, done) ->
-	done null,
-		email: email
-
-
+# development only
+if 'development' == app.get('env')
+	app.use express.errorHandler()
+auth = authentication.auth
 
 # set up repository
-app.set "store", (->
+app.set 'store', do ->
 	StoreClass = undefined
 	engine = config.store.engine
 	appStore = undefined
-	switch engine
-		when "git"
-		else
-			StoreClass = store.GitStore
-	
+
 	# Create new store for the app
-	appStore = new StoreClass(config.store.path)
-	unless appStore
-		
+	appStore = revisionary.Store.create(config.engine ? 'git', config.store.path)
+	if !appStore
 		# TODO: handle error
-		console.log "Failed to create store for application with engine '" + engine + "'. Check config.json."
-		throw new Error()
-	
+		console.log 'Failed to create store for application with engine \'' + engine + '\'. Check config.json.'
+		throw new Error
+
 	# If the appStore was created successfully, detect whether the 
 	# repository needs to be initialized and if so, do so.
 	appStore.detect (err, valid) ->
 		if err
-			
 			# TODO: handle error
 			console.log err
 			throw err
-		unless valid
+		if !valid
 			appStore.initialize null, (err) ->
 				if err
-					
 					# TODO: handle error
 					console.log err
 					throw err
-
-
+				return
+		return
 	appStore
-)()
+app.pages = pages
+
 routes = routes(app)
+pages.init app,
+	middleware: [ auth('html') ]
+	tools: [
+		{
+			route: '/pages/[page]/view'
+			action: routes.page.view
+		}
+		{
+			route: '/pages/[page]/diff/{v1}/{v2}'
+			action: routes.page.diff
+		}
+		{
+			route: '/pages/[page]/edit'
+			action: routes.page.editor
+		}
+		{
+			route: '/pages/[page]/edit'
+			action: routes.page.edit
+			method: 'post'
+		}
+		{
+			route: '/pages/[page]/remove'
+			action: routes.page.remove
+		}
+		{
+			route: '/pages/[page]/remove'
+			action: routes.page.remove
+			method: 'post'
+		}
+		{
+			route: '/pages/[page]/move'
+			action: routes.page.move
+		}
+		{
+			route: '/pages/[page]/move'
+			action: routes.page.move
+			method: 'post'
+		}
+		{
+			route: '/pages/[page]/log'
+			action: routes.page.log
+		}
+		{
+			route: '/pages/[page]/print'
+			action: routes.page.print
+		}
+		{
+			route: '/pages/[page]/export/{format}'
+			action: routes.page.export
+		}
+		{
+			route: '/pages/[page]/raw'
+			action: routes.page.raw
+		}
+		{
+			route: '/files/[page]'
+			action: routes.page.raw
+		}
+		# {
+		#	route: '/pages/[page]'
+		#	action: routes.page.view
+		# }
 
-# configure routes
-app.get "/login", (req, res, next) ->
-	res.render "login.jade"
+	]
+	views: [
+		{
+			filter: '*.{jpg,png,gif,tiff}'
+			action: routes.page.views['image']
+		}
+		{
+			filter: '*'
+			action: routes.page.views['default']
+		}
+	]
+	editors: [ {
+		filter: '*'
+		action: routes.page.editors['default']
+	} ]
+app.get '/', routes.index
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/view$/, auth('html'), routes.page.view);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/view\/([\w]+)$/, auth('html'), routes.page.view);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/diff\/([\w]+)\/([\w]+)$/, auth('html'), routes.page.diff);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/edit$/, auth('html'), routes.page.editor);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/log$/, auth('html'), routes.page.log);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/print$/, auth('html'), routes.page.print);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/export\/([\w\. ]+)$/, auth('html'), routes.page.export);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/raw$/, auth('html'), routes.page.raw);
+# app.get(/^\/files\/([\w\.\/%\(\)\{\}\[\] ]+)\/?/, auth('html'), routes.page.raw);
+# app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/edit$/, auth('html'), routes.page.edit);
+# app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/remove$/, auth('html'), routes.page.remove);
+# app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/move$/, auth('html'), routes.page.move);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss$/, auth('html'), routes.page.discuss.view);
+# app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss\/edit$/, auth('html'), routes.page.discuss.editor);
+# app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss\/edit$/, auth('html'), routes.page.discuss.edit);
+app.get '/categories', auth('html'), routes.categories.list
+app.get /^\/categories\/([\w\.\/ ]+)/, auth('html'), routes.categories.view
+app.get '/upload', auth('html'), routes.special.uploader
+app.post '/upload', auth('html'), routes.special.upload
+app.get '/recent', auth('html'), routes.special.recent
+app.get '/all', auth('html'), routes.special.all
+app.post '/go', auth('html'), routes.special.go
+app.post '/search', auth('html'), routes.special.search
+app.get '/api/search', auth('json'), routes.api.search
+app.get '/api/all', auth('json'), routes.api.all
+app.get '/api/index', auth('json'), routes.api.index
+app.post '/api/preview', auth('json'), routes.api.preview
+app.post '/api/upload', auth('json'), routes.api.upload
+http.createServer(app).listen app.get('port'), ->
+	console.log 'Express server listening on port ' + app.get('port')
+	return
 
-app.post "/login", passport.authenticate("local",
-	successRedirect: "/"
-	failureRedirect: "/login"
-	failureFlash: true
-)
-
-app.get "/", routes.index
-app.get /^\/pages\/([\w\.\/% ]+)\/view$/, auth("html"), routes.page.view
-app.get /^\/pages\/([\w\.\/% ]+)\/view\/([\w]+)$/, auth("html"), routes.page.view
-app.get /^\/pages\/([\w\.\/% ]+)\/diff\/([\w]+)\/([\w]+)$/, auth("html"), routes.page.diff
-app.get /^\/pages\/([\w\.\/% ]+)\/edit$/, auth("html"), routes.page.editor
-app.get /^\/pages\/([\w\.\/% ]+)\/log$/, auth("html"), routes.page.log
-app.get /^\/pages\/([\w\.\/% ]+)\/print$/, auth("html"), routes.page.print
-app.get /^\/pages\/([\w\.\/% ]+)\/export\/([\w\. ]+)$/, auth("html"), routes.page.export
-app.get /^\/pages\/([\w\.\/% ]+)\/raw$/, auth("html"), routes.page.raw
-app.get /^\/files\/([\w\.\/% ]+)\/?/, auth("html"), routes.page.raw
-app.post /^\/pages\/([\w\.\/% ]+)\/edit$/, auth("html"), routes.page.edit
-app.post /^\/pages\/([\w\.\/% ]+)\/remove$/, auth("html"), routes.page.remove
-app.post /^\/pages\/([\w\.\/% ]+)\/move$/, auth("html"), routes.page.move
-app.get /^\/pages\/([\w\.\/% ]+)\/discuss$/, auth("html"), routes.page.discuss.view
-app.get /^\/pages\/([\w\.\/% ]+)\/discuss\/edit$/, auth("html"), routes.page.discuss.editor
-app.post /^\/pages\/([\w\.\/% ]+)\/discuss\/edit$/, auth("html"), routes.page.discuss.edit
-app.get "/categories", auth("html"), routes.categories.list
-app.get /^\/categories\/([\w\.\/ ]+)/, auth("html"), routes.categories.view
-app.get "/upload", auth("html"), routes.special.uploader
-app.post "/upload", auth("html"), routes.special.upload
-app.get "/recent", auth("html"), routes.special.recent
-app.get "/all", auth("html"), routes.special.all
-app.post "/go", auth("html"), routes.special.go
-app.post "/search", auth("html"), routes.special.search
-http.createServer(app).listen app.get("port"), ->
-	console.log "Express server listening on port " + app.get("port")
+# ---
+# generated by js2coffee 2.0.1
--- app.js
+++ app.js
@@ -1,225 +1,230 @@
-
 /**
- * Module dependencies.
- */
-
-var express = require('express')
-  , pages = require('./core/page')
-  , routes = require('./routes')
-  , config = require('./config/config')
-
-  , expressValidator = require('express-validator')
-  , bunyan = require('bunyan')
-  , _ = require('underscore')
-  , moment = require('moment')
-  , passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy
+# Module dependencies.
+*/
 
 
-  , store = require('revisionary')
-  , wiki = require('wiki')
-  , markup = require('markup')
-  , authentication = require('./core/auth')
+(function() {
+  var LocalStrategy, app, auth, authentication, bunyan, config, express, expressValidator, http, markup, moment, pages, passport, path, revisionary, routes, wiki, _;
 
-  , http = require('http')
-  , path = require('path');
+  http = require('http');
 
-var app = express();
+  path = require('path');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+  express = require('express');
 
-app.set('log',bunyan.createLogger({name: "wiki", streams: [{
-        path: 'app.log',
-    },{
-    	stream: process.stderr,
-    	level: 'info'
-    }]
-}))
+  _ = require('underscore');
 
-// view helpers
-app.locals({
-	_ : _,
-	wiki: wiki,
-	markup: markup,
-	moment: moment,
-})
+  moment = require('moment');
 
+  passport = require('passport');
 
-// middleware
-app.use(express.favicon());
-app.use(express.logger('dev'));
+  LocalStrategy = require('passport-local').Strategy;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
-app.use(expressValidator());
+  bunyan = require('bunyan');
 
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+  expressValidator = require('express-validator');
 
-authentication.configure(app)
+  pages = require('./core/page');
 
-app.use(app.router);
-app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  routes = require('./routes');
 
+  config = require('./config/config');
 
-// normal error-handling middleware
-app.use(function(err, req, res, next) {
-	app.get('log').error(err);
-	res.render('error.jade', err)
-})
+  revisionary = require('revisionary');
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+  wiki = require('wiki');
 
+  markup = require('markup');
 
-auth = authentication.auth
+  authentication = require('./core/auth');
 
-// set up repository
-app.set('store', (function() {
-	var StoreClass, engine = config.store.engine, appStore;
-	
-	switch(engine) {
-		case 'git':
-		default:
-			StoreClass = store.GitStore;
-	}
+  app = express();
 
-	// Create new store for the app
-	appStore = new StoreClass(config.store.path);
+  app.set('port', process.env.PORT || 3000);
 
+  app.set('views', __dirname + '/views');
 
-	if(!appStore) {
-		// TODO: handle error
-		console.log("Failed to create store for application with engine '"+engine+"'. Check config.json.")
-		throw new Error();
-	}
+  app.set('view engine', 'jade');
 
-	// If the appStore was created successfully, detect whether the 
-	// repository needs to be initialized and if so, do so.
-	appStore.detect(function(err,valid) {
-		if(err) {
-			// TODO: handle error
-			console.log(err)
-			throw err;
-		}
-		if(!valid) {
-			appStore.initialize(null, function(err) {
-				if(err) {
-					// TODO: handle error
-					console.log(err)
-					throw err;
-				}
+  app.set('log', bunyan.createLogger({
+    name: 'wiki',
+    streams: [
+      {
+        path: 'app.log'
+      }, {
+        stream: process.stderr,
+        level: 'info'
+      }
+    ]
+  }));
 
-			})
-		}
-	})
-	
-	return appStore;
-})() )
+  app.locals({
+    _: _,
+    wiki: wiki,
+    markup: markup,
+    moment: moment
+  });
 
+  app.use(express.favicon());
 
-app.pages = pages
+  app.use(express.logger('dev'));
 
-routes = routes(app)
+  app.use(express["static"](path.join(__dirname, 'public')));
 
-pages.init(app,{
-	middleware: [auth('html')],
-	tools: [{
-		route: '/pages/[page]/view',
-		action: routes.page.view
-	},{
-		route: '/pages/[page]/diff/{v1}/{v2}',
-		action: routes.page.diff
-	},{
-		route: '/pages/[page]/edit',
-		action: routes.page.editor
-	},{
-		route: '/pages/[page]/edit',
-		action: routes.page.edit,
-		method: 'post'
-	},{
-		route: '/pages/[page]/log',
-		action: routes.page.log
-	},{
-		route: '/pages/[page]/print',
-		action: routes.page.print
-	},{
-		route: '/pages/[page]/export/{format}',
-		action: routes.page.export
-	},{
-		route: '/pages/[page]/raw',
-		action: routes.page.raw
-	},{
-		route: '/files/[page]',
-		action: routes.page.raw
-	}],
+  app.use(express.bodyParser());
 
-	views: [{
-		filter: '*.{jpg,png,gif,tiff}',
-		action: routes.page.views['image']
-	},{
-		filter: '*',
-		action: routes.page.views['default']
-	}],
+  app.use(expressValidator());
 
-	editors: [{
-		filter: '*',
-		action: routes.page.editors['default']
-	}]
+  app.use(express.methodOverride());
 
-})
+  app.use(express.cookieParser('your secret here'));
 
-app.get('/', routes.index);
+  app.use(express.session());
 
+  authentication.configure(app);
 
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/view$/, auth('html'), routes.page.view);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/view\/([\w]+)$/, auth('html'), routes.page.view);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/diff\/([\w]+)\/([\w]+)$/, auth('html'), routes.page.diff);
+  app.use(app.router);
 
+  app.use(require('less-middleware')({
+    src: __dirname + '/public'
+  }));
 
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/edit$/, auth('html'), routes.page.editor);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/log$/, auth('html'), routes.page.log);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/print$/, auth('html'), routes.page.print);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/export\/([\w\. ]+)$/, auth('html'), routes.page.export);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/raw$/, auth('html'), routes.page.raw);
-// app.get(/^\/files\/([\w\.\/%\(\)\{\}\[\] ]+)\/?/, auth('html'), routes.page.raw);
+  app.use(function(err, req, res, next) {
+    app.get('log').error(err);
+    res.render('error.jade', err);
+  });
 
+  if ('development' === app.get('env')) {
+    app.use(express.errorHandler());
+  }
 
+  auth = authentication.auth;
 
-// app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/edit$/, auth('html'), routes.page.edit);
-// app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/remove$/, auth('html'), routes.page.remove);
-// app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/move$/, auth('html'), routes.page.move);
+  app.set('store', (function() {
+    var StoreClass, appStore, engine, _ref;
+    StoreClass = void 0;
+    engine = config.store.engine;
+    appStore = void 0;
+    appStore = revisionary.Store.create((_ref = config.engine) != null ? _ref : 'git', config.store.path);
+    if (!appStore) {
+      console.log('Failed to create store for application with engine \'' + engine + '\'. Check config.json.');
+      throw new Error;
+    }
+    appStore.detect(function(err, valid) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      if (!valid) {
+        appStore.initialize(null, function(err) {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        });
+      }
+    });
+    return appStore;
+  })());
 
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss$/, auth('html'), routes.page.discuss.view);
-// app.get(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss\/edit$/, auth('html'), routes.page.discuss.editor);
-// app.post(/^\/pages\/([\w\.\/%\(\)\{\}\[\] ]+)\/discuss\/edit$/, auth('html'), routes.page.discuss.edit);
+  app.pages = pages;
 
-app.get('/categories', auth('html'), routes.categories.list)
-app.get(/^\/categories\/([\w\.\/ ]+)/, auth('html'), routes.categories.view)
+  routes = routes(app);
 
-app.get('/upload', auth('html'),routes.special.uploader)
-app.post('/upload', auth('html'),routes.special.upload)
+  pages.init(app, {
+    middleware: [auth('html')],
+    tools: [
+      {
+        route: '/pages/[page]/view',
+        action: routes.page.view
+      }, {
+        route: '/pages/[page]/diff/{v1}/{v2}',
+        action: routes.page.diff
+      }, {
+        route: '/pages/[page]/edit',
+        action: routes.page.editor
+      }, {
+        route: '/pages/[page]/edit',
+        action: routes.page.edit,
+        method: 'post'
+      }, {
+        route: '/pages/[page]/remove',
+        action: routes.page.remove
+      }, {
+        route: '/pages/[page]/remove',
+        action: routes.page.remove,
+        method: 'post'
+      }, {
+        route: '/pages/[page]/move',
+        action: routes.page.move
+      }, {
+        route: '/pages/[page]/move',
+        action: routes.page.move,
+        method: 'post'
+      }, {
+        route: '/pages/[page]/log',
+        action: routes.page.log
+      }, {
+        route: '/pages/[page]/print',
+        action: routes.page.print
+      }, {
+        route: '/pages/[page]/export/{format}',
+        action: routes.page["export"]
+      }, {
+        route: '/pages/[page]/raw',
+        action: routes.page.raw
+      }, {
+        route: '/files/[page]',
+        action: routes.page.raw
+      }
+    ],
+    views: [
+      {
+        filter: '*.{jpg,png,gif,tiff}',
+        action: routes.page.views['image']
+      }, {
+        filter: '*',
+        action: routes.page.views['default']
+      }
+    ],
+    editors: [
+      {
+        filter: '*',
+        action: routes.page.editors['default']
+      }
+    ]
+  });
 
+  app.get('/', routes.index);
 
-app.get('/recent', auth('html'), routes.special.recent)
-app.get('/all', auth('html'), routes.special.all)
-app.post('/go', auth('html'), routes.special.go)
-app.post('/search', auth('html'), routes.special.search)
+  app.get('/categories', auth('html'), routes.categories.list);
 
-app.get('/api/search',auth('json'), routes.api.search)
-app.get('/api/all',auth('json'), routes.api.all)
-app.get('/api/index',auth('json'), routes.api.index)
+  app.get(/^\/categories\/([\w\.\/ ]+)/, auth('html'), routes.categories.view);
 
-app.post('/api/preview',auth('json'), routes.api.preview)
-app.post('/api/upload',auth('json'), routes.api.upload)
+  app.get('/upload', auth('html'), routes.special.uploader);
 
+  app.post('/upload', auth('html'), routes.special.upload);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+  app.get('/recent', auth('html'), routes.special.recent);
+
+  app.get('/all', auth('html'), routes.special.all);
+
+  app.post('/go', auth('html'), routes.special.go);
+
+  app.post('/search', auth('html'), routes.special.search);
+
+  app.get('/api/search', auth('json'), routes.api.search);
+
+  app.get('/api/all', auth('json'), routes.api.all);
+
+  app.get('/api/index', auth('json'), routes.api.index);
+
+  app.post('/api/preview', auth('json'), routes.api.preview);
+
+  app.post('/api/upload', auth('json'), routes.api.upload);
+
+  http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+
+}).call(this);
